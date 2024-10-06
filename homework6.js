c78af207b9c6ddf52317c118256f21ecb94f461a
ingegerda84 <ingegerda84@gmail.com>
2024-10-06 10:44:31 +0200

    Homework6

---
+++ homework6.js
@@ -0,0 +1,65 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+      {
+        name: 'Клієнт 1',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків',
+        partners: [
+          {
+            name: 'Клієнт 1.1',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+          },
+          {
+            name: 'Клієнт 1.2',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+              {
+                name: 'Клієнт 1.2.3',
+                type: 'subSubCompany',
+                uses: 'Рішення для продажу квитків',
+                sells: 'Рішення для продажу квитків',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Клієнт 2',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків'
+      }
+    ]
+  };
+
+  function findValueByKey(object, companyName) {
+    if (object.name === companyName) {
+        console.log(Object.values(object));
+        return object;
+    }
+
+    for (let key in object) {
+        if (Array.isArray(object[key])) {
+            for (let item of object[key]) {
+                const result = findValueByKey(item, companyName);
+                if (result) return result;
+            }
+        }
+    }
+
+    return null;
+}
+
+const result = findValueByKey(company, "Клієнт 1");
+if (!result) {
+    console.log("Не знайдена така фірма");
+}
